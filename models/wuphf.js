7ed1eab4027a18fa9313c3affe794e5284f4524f
Luke2theDuke <57816716+Luke2theDuke@users.nopreply.github.com>
2020-02-06 14:25:11 -0600

    setup models and fixed login buttons

---
+++ models/wuphf.js
@@ -0,0 +1,27 @@
+// Creating our Wuphf (comments) model
+module.exports = function (sequelize, DataTypes) {
+    var Wuphf = sequelize.define("Wuphf", {
+        user: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return Wuphf;
+};
+
+// comments belong to a user and a user has many comments
+// video has many comments'
+
+// on video
+// api request get all the comments and include all the users
+
+// post author blog example, little different because we have multiple relations on the comment fields,  still only have one asscoaite method but it only 

efff28fc95ceae3e8459ac4939712654fb665e41
Luke2theDuke <57816716+Luke2theDuke@users.nopreply.github.com>
2020-02-09 12:33:30 -0600

    playing with jquery comments library

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
-    var blogContainer = $(".blog-container");
+    var videoContainer = $(".video-container");
 
 
     // "Welcome, xxxxx@xx.xxx!" at top of members.js
@@ -16,55 +16,46 @@ $(document).ready(function () {
         $.get("/api/videos", function (data) {
             console.log("Videos", data);
             videos = data;
-            if (!videos || !videos.length) {
-                console.log(" NO VIDEOS HENRY ");
-            }
-            else {
-                initializeRows();
-            }
+            initializeVideos();
         });
     }
 
-    function initializeRows() {
-        blogContainer.empty();
+    function initializeVideos() {
+        videoContainer.empty();
         var videosToAdd = [];
         for (var i = 0; i < videos.length; i++) {
             videosToAdd.push(createNewRow(videos[i]));
         }
-        blogContainer.prepend(videosToAdd);
+        videoContainer.before(videosToAdd);
 
 
     }
 
-    // This function constructs a post's HTML
     function createNewRow(video) {
 
-        var newPostCard = $("<div>");
-        newPostCard.addClass("cardvideo tile tile2 is-parent is-6 is-vertical has-text-centered");
-        var newPostCardBody = $("<article>");
-        newPostCardBody.addClass("tile is-child");
-        var newPostCardHeading = $("<div>");
-        // newPostCardHeading.addClass("card-header");
-        var newPostTitle = $("<p>");
-        newPostTitle.addClass("is-size-4 has-text-weight-bold");
-        // var newPostTitle2 = $("<a>");
-        // newPostTitle2.attr("href", "/video");
-        // newPostTitle2.addClass("my-link");
-        newPostTitle.text(video.title);
+        var videoCard = $("<section class='cardvideo column is-one-quarter has-text-centered'>");
+        var videoCardBody = $("<div>");
+        var videoCardHeading = $("<div>");
+        var videoTitle = $("<p class='is-size-4 has-text-weight-bold'>");
+        videoTitle.text(video.title);
 
-        var newPostBody = $("<p>");
-        var newPostBody2 = $("<p>");
+        var videoBody = $("<p>");
+        videoBody.text("Recorded on: " + video.rec_date);
 
-        newPostBody.text("Recorded on: " + video.rec_date);
-        newPostBody2.text("Keywords: " + video.keyword);
-        newPostCardHeading.append(newPostTitle);
-        // newPostTitle.append(newPostTitle2);
-        newPostCardBody.append(newPostBody);
-        newPostCardBody.append(newPostBody2);
-        newPostCard.append(newPostCardHeading);
-        newPostCard.append(newPostCardBody);
-        newPostCard.attr("id", video.id)
-        return newPostCard;
+        var videoBody2 = $("<p>");
+        videoBody2.text("Keyword: " + video.keyword);
+
+
+        videoCardHeading.append(videoTitle);
+
+        videoCardBody.append(videoBody);
+        videoCardBody.append(videoBody2);
+        videoCard.append(videoCardHeading);
+        videoCard.append(videoCardBody);
+        var playButton = $("<img src='../img/play.png' class='playButton'>");
+        videoCard.append(playButton);
+        videoCard.attr("id", video.id);
+        return videoCard;
     }
     $(document).on("click", ".cardvideo", function (e) {
         const id = $(this).attr("id");
@@ -74,10 +65,10 @@ $(document).ready(function () {
 });
 
 
- // $(".my-link").on("click", function (event) {
-        //     event.preventDefault();
-        //     var videoId = $(this).attr("data-video");
-        //     $.get(`/video/${videoId}`, function (response) {
-        //         console.log(response)
-        //     });
-        // });
+// $(".my-link").on("click", function (event) {
+//     event.preventDefault();
+//     var videoId = $(this).attr("data-video");
+//     $.get(`/video/${videoId}`, function (response) {
+//         console.log(response)
+//     });
+// });

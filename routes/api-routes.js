f76313c5e5a1ed71b9b45f0e21f271319d405a8e
Luke2theDuke <57816716+Luke2theDuke@users.nopreply.github.com>
2020-02-10 09:56:22 -0600

    youtube results working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,8 @@
 // Requiring our models and passport as we've configured it
+require("dotenv").config();
 var db = require("../models");
 var passport = require("../config/passport");
-
-var Wuphf = require("../models/wuphf.js");
-
+const axios = require("axios").default;
 module.exports = function (app) {
     // Using the passport.authenticate middleware with our local strategy.
     // If the user has valid login credentials, send them to the members page.
@@ -11,7 +10,6 @@ module.exports = function (app) {
     app.post("/api/login", passport.authenticate("local"), function (req, res) {
         res.json(req.user);
     });
-
     // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
     // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
     // otherwise send back an error
@@ -27,13 +25,11 @@ module.exports = function (app) {
                 res.status(401).json(err);
             });
     });
-
     // Route for logging user out
     app.get("/logout", function (req, res) {
         req.logout();
         res.redirect("/");
     });
-
     // Route for getting some data about our user to be used client side
     app.get("/api/user_data", function (req, res) {
         if (!req.user) {
@@ -48,27 +44,19 @@ module.exports = function (app) {
             });
         }
     });
-    app.get("/api/all", function (req, res) {
-        // Finding all Wuphfs, and then returning them to the user as JSON.
-        // Sequelize queries are asynchronous, which helps with perceived speed.
-        // If we want something to be guaranteed to happen after the query, we'll use
-        // the .then function
-        Wuphf.findAll({}).then(function (results) {
-            // results are available to us inside the .then
-            res.json(results);
-        });
-    });
-    // Add a wuphf
-    app.post("/api/new", function (req, res) {
-        console.log("Wuphf Data:");
-        console.log(req.body);
-        Wuphf.create({
-            body: req.body.body,
-            created_at: req.body.created_at
-        }).then(function () {
-            res.end();
-        });
-    });
+    // app.get("/api/video/:id", function (req, res) {
+    //     console.log("Hello");
+    //     db.Video.findOne({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     }).then(function (result) {
+    //         res.json(result.data);
+    //     }).catch(err => {
+    //         console.log(err);
+    //         res.json(err);
+    //     });
+    // });
     app.get("/api/video/:id", function (req, res) {
         db.Video.findOne({
             where: {
@@ -78,21 +66,10 @@ module.exports = function (app) {
             res.json(result);
         });
     });
-
-    app.post("/api/addVid", function (req, res) {
-        db.Video.create({
-                title: req.body.title,
-                video_url: req.body.video_url,
-                rec_date: req.body.rec_date,
-                keyword: req.body.keyword
-            })
-            .then(function () {
-                res.redirect(307, "/api/login");
-            })
-            .catch(function (err) {
-                res.status(401).json(err);
-            });
+    app.get("/api/video/search/:keyword", function (req, res) {
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${res}&type=video&key=${process.env.key}`).then(function (response) {
+            console.log(response);
+            res.json(response.data);
+        }).catch(response => res.json(response));
     });
-
-
 };
